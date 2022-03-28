a575346eb00af4088146ade3cacc846c1d5ed2f5
neo0112358 <borjaruizreverter@gmail.com>
2022-03-28 14:24:45 -0300

    New build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cdf4368163d362bd2f4b6c084868c22.js"
+  "/precache-manifest.70845a23bec44b0c19ebaf680a49322d.js"
 );
 
 workbox.clientsClaim();
