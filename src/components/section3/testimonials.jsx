c30f6e538d3ad4bcfc22503acafaac94f4b50ffa
neo0112358 <borjaruizreverter@gmail.com>
2022-03-28 11:51:59 -0300

    Carousel opiniones

--- src/components/section3/testimonials.jsx
+++ src/components/section3/testimonials.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import {Container,Row,Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import Img1 from './img/testimonials.png';
 import './style.css'
 
-function Testimonials() {
-    const { t } = useTranslation()
-  return (
-    <>
-    <Container fluid className='mt-5 mb-5'>
-        <Row > 
-        <Col className="mt-5"> 
-            <p className="h-title bold mb-0" >{t('testimonials.title')}</p>
-            <div id="indicator3"></div>   
-            </Col>
-            <Col sm={12}>
-            <img className="testimonials mt-5 mb-5" src= {Img1} alt="Dish of the day"/>
-            </Col>
+import CarouNuka from './carousel-opiniones.jsx';
 
-        </Row>
+function Testimonials() {
+  const { t } = useTranslation()
+  return (
+    <Container className='mt-5 mb-5'>
+      <Row >
+        <Col className="mt-5">
+          <p className="h-title bold mb-0" >{t('testimonials.title')}</p>
+          <div id="indicator3"></div>
+        </Col>
+        <Col sm={12} className="">
+          <CarouNuka />
+        </Col>
+      </Row>
     </Container>
-    </>
   )
 }
 
